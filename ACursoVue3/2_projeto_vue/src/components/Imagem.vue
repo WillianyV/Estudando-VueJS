2e7e906adb59c6eb0da6a7df3a5f01f8054919c7
WillianyV <41123970+WillianyV@users.noreply.github.com>
2022-04-30 10:20:44 -0300

    Diretivas e Atributos
    Diretirvas- maioria começa com 'V';
    
    podemos passar os atributos que veem do BD, para as telas atraves das diretivas

---
+++ ACursoVue3/2_projeto_vue/src/components/Imagem.vue
@@ -0,0 +1,14 @@
+<template>
+    <img :src="imagem_src" :alt="descricao">
+</template>
+<script>
+export default {
+    name:'Imagem',
+    data() {
+        return {
+            imagem_src:"/img/avatar.png",
+            descricao:'Williany Veras'
+        }
+    }
+}
+</script>
--- ACursoVue3/2_projeto_vue/src/components/Info.vue
+++ ACursoVue3/2_projeto_vue/src/components/Info.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <p>Estou trabalhando no momento.</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oportunidades.</p>
     <p>Utilzo as seguintes tecnologias</p>
     <ul>
       <li>JavaScript</li>
@@ -8,11 +9,27 @@
       <li>Laravel</li>
       <li>PostgreSQL</li>
     </ul>
+    <p v-if="4 > 2">Testando</p>
+    <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+    <p>Para acessar meu portfólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+    <Imagem />
   </div>
 </template>
 
 <script>
+import Imagem from './Imagem.vue'
 export default {
   name: "Info",
+  components: {
+    Imagem,
+  },
+  data() {
+    return {
+      esta_trabalhando: false,
+      mostrar_email: false,
+      email:'williany.veras@gmai.com',
+      meu_link:'https://www.google.com/'
+    }
+  }
 };
 </script>
