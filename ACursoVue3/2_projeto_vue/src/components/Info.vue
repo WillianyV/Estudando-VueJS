af262429d4ee9303a20c52cf233d54c1574af0c7
WillianyV <41123970+WillianyV@users.noreply.github.com>
2022-04-30 09:20:39 -0300

    Hieraquia dos componentes

---
+++ ACursoVue3/2_projeto_vue/src/components/Info.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Estou trabalhando no momento.</p>
+    <p>Utilzo as seguintes tecnologias</p>
+    <ul>
+      <li>JavaScript</li>
+      <li>PHP</li>
+      <li>Laravel</li>
+      <li>PostgreSQL</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Info",
+};
+</script>
