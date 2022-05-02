afaf9419fda7754e91f96953f64b25ae122f06ed
WillianyV <41123970+WillianyV@users.noreply.github.com>
2022-05-02 15:58:33 -0300

    eventos e multiplos eventos

--- ACursoVue3/2_projeto_vue/src/App.vue
+++ ACursoVue3/2_projeto_vue/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
+    <Header />
     <PrimeiroComponente />
-    <p>O dado de App Vue é: {{ testando }}</p>
+    <p class="paragrafo-pai">O dado de App Vue é: {{ testando }}</p>
     <life-cycle></life-cycle> <!-- Também o componente pode ser chamado dessa forma, mas usada <PrimeiroComponente /> -->
     <Pessoa />
     <Form />
@@ -9,18 +10,20 @@
 </template>
 
 <script>
+  import Header from './components/Header.vue'
   import PrimeiroComponente from './components/PrimeiroComponente.vue'
   import LifeCycle from './components/LifeCycle.vue'
   import Pessoa from './components/Pessoa.vue'
-  import Form from './components/Form.vue'
+  import Form from './components/Form.vue'  
 
   export default {
     name:"app",
     components:{
+      Header,
       PrimeiroComponente,
       LifeCycle,
       Pessoa,
-      Form
+      Form,      
     },
     data() {
       return {
@@ -29,4 +32,19 @@
     }
   }
 </script>
+
+<style>
+  body{
+    background-color: #333;
+    color: #FFF;
+  }
+
+  a{
+    color: red;
+  }
+
+  .teste{
+   background-color: black; 
+  }
+</style>
 
---
+++ ACursoVue3/2_projeto_vue/src/components/MultiposEventos.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <button @click="primeiro(txt_primeiro,event),segundo(txt_segundo,event),terceiro(event)">Ativar múltiplos eventos</button>
+        <p>{{ multiplos_eventos }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'MultiposEventos',
+    data() {
+        return {
+            multiplos_eventos:"",
+            txt_primeiro:"Primeiro evento",
+            txt_segundo:"Segundo evento",
+        }
+    },
+    methods: {
+        primeiro(txt, e){
+            this.multiplos_eventos = txt
+        },
+        segundo(txt, e){
+            setTimeout(() => this.multiplos_eventos = txt,500)
+        },
+        terceiro(e){
+            setTimeout(() => this.multiplos_eventos = "",1500)
+        }
+    }
+}
+</script>
+<style scoped>
+    div{
+        margin: 100px;
+    }
+</style>
