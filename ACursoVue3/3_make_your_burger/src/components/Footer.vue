6dc948f4096f404b1fe3d1dc60c5705e4f46af8a
WillianyV <41123970+WillianyV@users.noreply.github.com>
2022-05-06 17:32:22 -0300

    Finalizando a Home/Form Burger

--- ACursoVue3/3_make_your_burger/src/components/Footer.vue
+++ ACursoVue3/3_make_your_burger/src/components/Footer.vue
@@ -1,23 +1,23 @@
 <template>
-    <div id="footer">
-        <p>Make Your Burger &copy; 2021</p>
-    </div>
+  <div id="footer">
+    <p>Make Your Burger &copy; 2021</p>
+  </div>
 </template>
 <script>
 export default {
-    name:"Fotter",
-}
+  name: "Fotter",
+};
 </script>
 <style scoped>
-    #footer {
-        height: 200px;
-        background-color: #222;
-        border-top: 4px solid #111;
-        color: #FCBA03;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        bottom: 0px;
-        width: 100%;
-    }
+#footer {
+  height: 200px;
+  background-color: #222;
+  /* border-top: 4px solid #111; */
+  color: #fcba03;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  bottom: 0px;
+  width: 100%;
+}
 </style>
--- ACursoVue3/3_make_your_burger/src/views/HomeView.vue
+++ ACursoVue3/3_make_your_burger/src/views/HomeView.vue
@@ -1,13 +1,21 @@
 <template>
+  <Banner />
   <div class="main-container">
-    <h1>HOME</h1>
+    <h1>Monte seu Burger:</h1>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
+import Banner from '../components/Banner.vue'
+import BurgerForm from '../components/BurgerForm.vue'
 
 export default {
   name: 'HomeView',
+  components: {
+    Banner,
+    BurgerForm
+  }
   
 }
 </script>
