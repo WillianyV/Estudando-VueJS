87e4311b9d94f852a0838555adcb6cb873423b88
WillianyV <41123970+WillianyV@users.noreply.github.com>
2022-11-30 16:53:33 -0300

    JWT - Finalizando o Back-end

--- BLocadoraDeCarros/app/Models/User.php
+++ BLocadoraDeCarros/app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -42,7 +43,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-     /**
+    /**
      * Get the identifier that will be stored in the subject claim of the JWT.
      *
      * @return mixed
